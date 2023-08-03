6a5aaf10ee161551bb034ba4539868aab7f4a7ac
Luciano Micucci <luchomicucci@gmail.com>
2023-08-03 16:53:04 -0300

    afterClass1
    Ejercicios sin resolver

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Mi app de React
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+  
+
+
+
+// Ejercicio 1: Lista de Compras
+
+// Crea un componente de lista de compras que permita al usuario
+// agregar y eliminar elementos de una lista.
+// Utiliza useState para administrar la lista de elementos.
+
+// Ejercicio 2: Lista de Tareas
+
+// Crea un componente de lista de tareas.
+// Permite al usuario agregar tareas nuevas a la lista y
+// marcarlas como completadas o eliminarlas. 
+// Utiliza useState para administrar la lista de tareas.
+
+// Ejercicio 3: Crear tres componentes
+// Componente 1: Componente contenedor con un array de productos
+// Componente 2: Componente de presentacion de producto
+// Componente 3: Componente de titulo personalizable por props **BONUS**
